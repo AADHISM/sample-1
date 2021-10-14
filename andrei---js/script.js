e15f49b600ff66b8f2fb7d1420f12897b20fe8c8
Aadhithiya <66451374+AADHISM@users.noreply.github.com>
2021-10-14 11:17:58 +0530

    Add files via upload

---
+++ andrei---js/script.js
@@ -0,0 +1,15 @@
+const array = [-1, 0, 3, 100, 99, 2, 99]; // should return 100
+const array2 = ["a", 3, 4, 2]; // should return 4
+const array3 = []; // should return 0
+
+function biggestNumberInArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function biggestNumberInArray2(arr) {}
+
+function biggestNumberInArray3(arr) {}
+
+biggestNumberInArray(array);
